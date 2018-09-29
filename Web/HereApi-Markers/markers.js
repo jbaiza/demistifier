9a0d2163d9ecfb43ff701d108f7ad9b40b423573
arturso <arturs.oniscenko@dpa.lv>
2018-09-29 04:54:37 +0300

    Web-Demo: Merge Reach distance with markers

--- Web/HereApi-Markers/markers.js
+++ Web/HereApi-Markers/markers.js
@@ -1,5 +1,12 @@
 //https://developer.here.com/api-explorer/geovisualization/technology_markers/markers-csv-provider
 
+var startPosition = '56.924012,24.137290';
+var departureTime = '2018-09-28T07:00:00';
+
+var SECONDS = 1;
+var MINUTES = 60 * SECONDS;
+var timeRange = 15 * MINUTES;
+
 (function () {
 'use strict';
 
@@ -33,15 +40,69 @@ var app_id = 'Wdwou7J9CcHwv9JKHUWp';      // <- replace with your own from
         defaultLayers.normal.base,
         {
             pixelRatio,
-            center: new H.geo.Point(52.3, 13.34),
-            zoom: 10
+            center: { lat: 56.924012, lng: 24.137290 },
+            zoom: 15
         }
     );
+
 
     // make the map interactive
     const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
     let ui = H.ui.UI.createDefault(map, layers);
     ui.removeControl('mapsettings');
+
+    // Get an instance of the routing service
+    var router = platform.getRoutingService();
+
+    // Set a new starting position (departure) when the user clicks the map
+    map.addEventListener('tap', function (evt) {
+        var coord = map.screenToGeo(evt.currentPointer.viewportX, evt.currentPointer.viewportY);
+        startPosition = coord.lat + ',' + coord.lng;
+        startIsolineRouting();
+    });
+
+    function startIsolineRouting() {
+        // Set up the Routing API parameters
+        var routingParams = {
+            'mode': 'fastest;car;traffic:enabled', // car, truck (only with type fastest), pedestrian
+            'start': startPosition,
+            'departure': departureTime,	// can also use 'arrival' or both
+            'rangetype': 'time', // distance (meters), time (seconds)
+            'range': timeRange
+        };
+
+        // Call the Routing API to calculate an isoline
+        router.calculateIsoline(
+            routingParams,
+            onResult,
+            function (error) {
+                alert(error.message);
+            });
+    }
+
+    var onResult = function (result) {
+        var center = new H.geo.Point(result.response.center.latitude, result.response.center.longitude),
+            isolineCoords = result.response.isoline[0].component[0].shape,
+            linestring = new H.geo.LineString(),
+            isolinePolygon,
+            isolineCenter;
+
+        // Add the returned isoline coordinates to a linestring
+        isolineCoords.forEach(function (coords) {
+            linestring.pushLatLngAlt.apply(linestring, coords.split(','));
+        });
+
+        // Create a polygon and a marker representing the isoline
+        isolinePolygon = new H.map.Polygon(linestring);
+        isolineCenter = new H.map.Marker(center);
+
+        // Add the polygon and marker to the map
+        map.addObjects([isolineCenter, isolinePolygon]);
+
+        // Center and zoom the map so that the whole isoline polygon is in the viewport:
+        map.setViewBounds(isolinePolygon.getBounds());
+    };
+
 
     // resize map on window resize
     window.addEventListener('resize', function () {
