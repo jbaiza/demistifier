acd2986b214d137494f2a1febbbb8eebb74e3a37
Jānis Baiža <jbaiza@gmail.com>
2018-09-29 14:01:13 +0300

    Add calculation tooltip

--- demistifier_app/app/helpers/application_helper.rb
+++ demistifier_app/app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def get_application_queue_priority_tooltip(application)
+    text = []
+    if application.sort_index & 128 == 128
+      text << "Desirable start date in past"
+    end
+    if application.priority_5years_old && application.priority_child_local
+      text << "Older than 5 years"
+    end
+    if application.priority_commission
+      text << "Priority by commission decision"
+    end
+    if application.priority_sibling
+      text << "Sibling in institution"
+    end
+    if application.priority_parent_local && application.priority_child_local
+      text << "Child and parents declared in Riga"
+    end
+    if application.priority_child_local
+      text << "Child declared in Riga"
+    end
+    unless application.priority_parent_local && application.priority_child_local
+      text << "Not declared in Riga"
+    end
+    text.join('&#013;')
+  end
+
 end
