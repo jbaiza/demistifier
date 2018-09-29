d0d7803b1eec879bf65c341a949622942ef4c09a
Jānis Baiža <jbaiza@gmail.com>
2018-09-29 12:25:15 +0300

    Add children queues information

--- demistifier_app/config/routes.rb
+++ demistifier_app/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :institutions, only: [:index] do
     resources :institution_program_languages, only: [:show]
   end
+  resources :children, only: [:show]
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
