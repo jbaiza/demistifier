b02a0d9fbdbc7db78a0c9afb0f8974cd3d6c1580
Rolands Bondars <r.bondars@gmail.com>
2018-09-29 17:48:35 +0300

    Add hints screen

--- demistifier_app/app/controllers/children_controller.rb
+++ demistifier_app/app/controllers/children_controller.rb
@@ -12,6 +12,9 @@ class ChildrenController < ApplicationController
     end
   end
 
+  def hint
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_child
