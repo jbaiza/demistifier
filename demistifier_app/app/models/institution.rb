30dd39f5db59c0cda9b659b19802c5c00d423574
Jānis Baiža <jbaiza@gmail.com>
2019-01-27 15:41:38 +0200

    Add HTTP to URL if it's not entered

--- demistifier_app/app/models/institution.rb
+++ demistifier_app/app/models/institution.rb
@@ -1,4 +1,10 @@
 class Institution < ApplicationRecord
   belongs_to :region
   has_many :institution_program_languages
+
+  def full_url
+    return unless url
+    return url if url.starts_with? "http"
+    "http://#{url}"
+  end
 end
